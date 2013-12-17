2c7397777875300a18175d1c0680210e54feac20
Zachary Lesko <lesko@physics.umn.edu>
2013-12-16 18:48:54 -0600

    so this is because I realized they measure  phi from negative, needed to deal with that

--- HCALDigis/src/HCALDigis.cc
+++ HCALDigis/src/HCALDigis.cc
@@ -23,7 +23,9 @@
 #include <vector>
 #include <map>
 #include <utility>
-
+#define _USE_MATH_DEFINES 
+#include <math.h>
+//not sure if this will work need to change this bloody into 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
@@ -69,8 +71,8 @@ private:
     virtual void endRun(edm::Run const&, edm::EventSetup const&);
     virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
     virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-    
-    int etatoieta ( double angle);
+
+    int etatoieta(double angle);
 
     // ----------member data ---------------------------
     TFile* tfile;
@@ -100,19 +102,19 @@ HCALDigis::HCALDigis(const edm::ParameterSet& iConfig)
 {
     edm::Service<TFileService> fs;
     TFileDirectory passed = fs->mkdir("pass");
-    
-    
+
+
     TFileDirectory notep = fs->mkdir("nepass");
     PhAvgDigi = passed.make<TH1F>("avgDigi", "avgDigi", 10, -0.5, 9.5);
     PhAvgadc = passed.make<TH1F>("avgAdc", "avgAdc", 100, -0.5, 99.5);
     PnehAvgDigi = notep.make<TH1F>("avgDigi", "avgDigi", 10, -0.5, 9.5);
     PnehAvgadc = notep.make<TH1F>("avgAdc", "avgAdc", 100, -0.5, 99.5);
     char ht[256];
-    
-    
+
+
     for(int i = 0; i < 13; i++) for(int j = 0; j < 36; j++) for(int k = 0; k < 2; k++)
             {
-        
+
                 if(i > 10 && j % 2 == 0) continue;
                 sprintf(ht, "digi_+%d_%d_%d", i + 29, j * 2 + 1, k + 1);
                 PhDigi[i][j][k] = passed.make<TH1F>(ht, ht, 10, -0.5, 9.5);
@@ -122,8 +124,8 @@ HCALDigis::HCALDigis(const edm::ParameterSet& iConfig)
                 Phadc[i][j][k] = passed.make<TH1F>(ht, ht, 100, -0.5, 99.5);
                 sprintf(ht, "adc_-%d_%d_%d", i + 29, j * 2 + 1, k + 1);
                 Phadc[i + 13][j][k] = passed.make<TH1F>(ht, ht, 100, -0.5, 99.5);
-                
-                
+
+
                 sprintf(ht, "digi_+%d_%d_%d", i + 29, j * 2 + 1, k + 1);
                 PnehDigi[i][j][k] = notep.make<TH1F>(ht, ht, 10, -0.5, 9.5);
                 sprintf(ht, "digi_-%d_%d_%d", i + 29, j * 2 + 1, k + 1);
@@ -133,10 +135,10 @@ HCALDigis::HCALDigis(const edm::ParameterSet& iConfig)
                 sprintf(ht, "adc_-%d_%d_%d", i + 29, j * 2 + 1, k + 1);
                 Pnehadc[i + 13][j][k] = notep.make<TH1F>(ht, ht, 100, -0.5, 99.5);
 
-                 
+
             }
-            
-           
+
+
 
     printDigi = iConfig.getUntrackedParameter<bool>("printDigi", false);
 
@@ -146,7 +148,7 @@ HCALDigis::~HCALDigis() {
 
     // do anything here that needs to be done at desctruction time
     // (e.g. close files, deallocate resources etc.)
- }
+}
 
 
 //
@@ -164,31 +166,38 @@ HCALDigis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     edm::Handle<HFDigiCollection> hf_digi;
     iEvent.getByType(hf_digi);
 
-    
+
     //vector <int> giphi;
     //map<pair<int, int>,  vector<reco::GenParticle*> > gparts; 
-    set<pair<int, int> >     gparts; //makes a set that contains locations of all generated electrons
+    set<pair<int, int> > gparts; //makes a set that contains locations of all generated electrons
     edm::Handle<reco::GenParticleCollection> genInfo;
     iEvent.getByLabel("genParticles", genInfo);
-            //genInfo.pdgid no
+    //genInfo.pdgid no
 
 
     for(unsigned ighit = 0; ighit < genInfo->size(); ++ighit)// so this function goes and figures out the location of all electron location to allow matching4
-    {   //int i; delete? no idea what this was supposed to do
+    { //int i; delete? no idea what this was supposed to do
         const reco::GenParticle& genin = (* genInfo)[ighit];
-        int giphi = (int) (genin.phi() + 10) / 10;
-        
-       
-        int gieta = etatoieta(genin.eta());//the function that gets ieta from eta(genin.eta());
+        int giphi;
+        if(genin.phi() >= 0)
+        {
+            giphi = ((int) ((genin.phi())*180 / M_PI) / 10)*2 + 1; //so this converts into degrees since everything is nicer there. followed by conversion it iphi
+        } else
+        {
+            giphi = ((int) ((genin.phi())*180 / M_PI + 360) / 10)*2 + 1;
+        }
+
+
+        int gieta = etatoieta(genin.eta()); //the function that gets ieta from eta(genin.eta());
         int pdgid = genin.pdgId();
-        
-        if (pdgid==11 && genin.numberOfDaughters() > 0) 
-        { 
-        
-        gparts.insert(make_pair(gieta, giphi));
-    }
-    
-    
+
+        if(pdgid == 11 && genin.numberOfDaughters() > 0)
+        {
+
+            gparts.insert(make_pair(gieta, giphi));
+        }
+
+
     }
 
 
@@ -212,29 +221,34 @@ HCALDigis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
         depth--;
 
         //fill digi histograms
-        if(gparts.find(make_pair(frame.id().ieta(),frame.id().iphi()))!=gparts.end())
+        if(gparts.find(make_pair(frame.id().ieta(), frame.id().iphi())) != gparts.end())
         //if(true)
         {
-        for(int isample = 0; isample < frame.size(); ++isample)//time in particulare. 5 times
-        {
-            int adc = frame[isample].adc();
-            PhAvgDigi->SetBinContent(isample + 1, PhAvgDigi->GetBinContent(isample + 1) + adc);
-            PhAvgadc->Fill(adc);
-            PhDigi[ieta][iphi][depth]->SetBinContent(isample + 1, PhDigi[ieta][iphi][depth]->GetBinContent(isample + 1) + adc);
-            Phadc[ieta][iphi][depth]->Fill(adc);
-        }
+            //cout << "so electron id has ieta=" << frame.id().ieta() << "  and a iphi of =" << frame.id().iphi() << endl;
+            for(int isample = 0; isample < frame.size(); ++isample)//time in particulare. 5 times
+            {
+                //ADC
+                int adc = frame[isample].adc();
+                PhAvgDigi->SetBinContent(isample + 1, PhAvgDigi->GetBinContent(isample + 1) + adc);
+                PhAvgadc->Fill(adc);
+                PhDigi[ieta][iphi][depth]->SetBinContent(isample + 1, PhDigi[ieta][iphi][depth]->GetBinContent(isample + 1) + adc);
+                Phadc[ieta][iphi][depth]->Fill(adc);
+            }
         }
         //so this following piece should be things that are not electrons that are passing at this moment. since right now I am not doing any cuts it is obviously a graph of all non electrons
-        if(gparts.find(make_pair(frame.id().ieta(),frame.id().iphi()))==gparts.end())
+        //if(gparts.find(make_pair(frame.id().ieta(), frame.id().iphi())) == gparts.end())
+        else
         {
-        for(int isample = 0; isample < frame.size(); ++isample)//ask joe about this?????????????????????
-        {  
-            int adc = frame[isample].adc();
-            PnehAvgDigi->SetBinContent(isample + 1, PnehAvgDigi->GetBinContent(isample + 1) + adc);
-            PnehAvgadc->Fill(adc);
-            PnehDigi[ieta][iphi][depth]->SetBinContent(isample + 1, PnehDigi[ieta][iphi][depth]->GetBinContent(isample + 1) + adc);
-            Pnehadc[ieta][iphi][depth]->Fill(adc);
-        }
+            for(int isample = 0; isample < frame.size(); ++isample)//ask joe about this?????????????????????
+            {
+
+                //linearadc? maybe...
+                int adc = frame[isample].adc();
+                PnehAvgDigi->SetBinContent(isample + 1, PnehAvgDigi->GetBinContent(isample + 1) + adc);
+                PnehAvgadc->Fill(adc);
+                PnehDigi[ieta][iphi][depth]->SetBinContent(isample + 1, PnehDigi[ieta][iphi][depth]->GetBinContent(isample + 1) + adc);
+                Pnehadc[ieta][iphi][depth]->Fill(adc);
+            }
         }
     }
 }
@@ -262,8 +276,8 @@ HCALDigis::endJob()
                     PhDigi[i + 13][j][k]->Scale(1 / PhDigi[i + 13][j][k]->GetEntries());
                 }
             }
-    
-    
+
+
     for(int i = 0; i < 13; i++) for(int j = 0; j < 36; j++) for(int k = 0; k < 2; k++)
             {
                 if(i > 10 && j % 2 == 0) continue;
@@ -312,32 +326,33 @@ HCALDigis::fillDescriptions(edm::ConfigurationDescriptions& descriptions)
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(HCALDigis);
-int HCALDigis::etatoieta ( double angle)
+
+int HCALDigis::etatoieta(double angle)
 {
-using namespace std;
-    int anglesign = (int) (fabs(angle)/angle);
+    using namespace std;
+    int anglesign = (int) (fabs(angle) / angle);
     angle = fabs(angle);
-    int tower=0;
-    
-    
-    if(2.853>angle) tower = 0;
-    else if (angle<=2.964) tower=29;
-    else if (angle<=3.139) tower=30;
-    else if (angle<=3.314) tower=31;
-    else if (angle<=3.489) tower=32;
-    else if (angle<=3.664) tower=33;
-    else if (angle<=3.839) tower=34;
-    else if (angle<=4.013) tower=35;
-    else if (angle<=4.191) tower=36;
-    else if (angle<=4.363) tower=37;
-    else if (angle<=4.538) tower=38;
-    else if (angle<=4.716) tower=39;
-    else if (angle<=4.889) tower=40;
-    else if (angle<=5.191) tower=41;
-    
-    tower*=anglesign;
-   
-    
-    
+    int tower = 0;
+
+
+    if(2.853 > angle) tower = 0;
+    else if(angle <= 2.964) tower = 29;
+    else if(angle <= 3.139) tower = 30;
+    else if(angle <= 3.314) tower = 31;
+    else if(angle <= 3.489) tower = 32;
+    else if(angle <= 3.664) tower = 33;
+    else if(angle <= 3.839) tower = 34;
+    else if(angle <= 4.013) tower = 35;
+    else if(angle <= 4.191) tower = 36;
+    else if(angle <= 4.363) tower = 37;
+    else if(angle <= 4.538) tower = 38;
+    else if(angle <= 4.716) tower = 39;
+    else if(angle <= 4.889) tower = 40;
+    else if(angle <= 5.191) tower = 41;
+
+    tower *= anglesign;
+
+
+
     return tower;
 }
